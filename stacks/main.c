da67a6a8210ff02d16d60762b0ecde617234926b
mikemeding <mikeymeding@gmail.com>
2013-10-07 13:44:31 -0400

    added all the project files and .gitignore -MM

---
+++ stacks/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: mike
+ *
+ * Created on October 7, 2013, 1:04 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+

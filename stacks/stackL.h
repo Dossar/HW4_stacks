07a8fa8936b962099984ce6245254fbca2044f46
mikemeding <mikeymeding@gmail.com>
2013-10-11 21:38:29 -0400

    Fixed all linked list stack issues. All functions operate correctly -MM

--- stacks/stackL.h
+++ stacks/stackL.h
@@ -15,86 +15,88 @@
 extern "C" {
 #endif
 
-   typedef struct Node {
-	  StackData data;
-	  struct Node *next;
-   } Node, *NodePtr;
+    typedef struct Node {
+        StackData data;
+        struct Node *next;
+    } Node, *NodePtr;
 
-   typedef struct stackType {
-	  NodePtr top;
-   } StackType, *Stack;
+    typedef struct stackType {
+        NodePtr top;
+    } StackType, *Stack;
 
 
 
-   /*PROTOTYPES*/
-   void push(Stack stack, StackData data);
+    /*PROTOTYPES*/
+    void push(Stack stack, StackData data);
 
-   StackData pop(Stack stack);
+    StackData pop(Stack stack);
 
-   StackData peek(Stack stack);
+    StackData peek(Stack stack);
 
-   Stack initStack(void);
+    Stack initStack(void);
 
-   void empty(Stack stack);
+    void empty(Stack stack);
 
-   bool isEmpty(Stack stack);
+    bool isEmpty(Stack stack);
 
-   /////////////////////////////////////////////
+    /////////////////////////////////////////////
 
-   /* create a new and empty stack */
-   Stack initStack(void) {
-	  Stack sp = (Stack) malloc(sizeof (StackType)); // allocate memory to 
-	  sp -> top = NULL;
-	  return sp;
-   }
+    /* create a new and empty stack */
+    Stack initStack(void) {
+        Stack sp = (Stack) malloc(sizeof (StackType)); // allocate memory to 
+        sp -> top = NULL;
+        return sp;
+    }
 
-   /* function will push a new value given by StackData to the stack*/
-   void push(Stack stack, StackData data) {
-	  NodePtr np = (NodePtr) malloc(sizeof (Node));
-	  np ->data = data;
-	  np ->next = stack->top;
-	  stack ->top = np;
-   }
+    /* function will push a new value given by StackData to the stack*/
+    void push(Stack stack, StackData data) {
+        NodePtr np = (NodePtr) malloc(sizeof (Node));
+        np ->data = data;
+        np ->next = stack->top;
+        stack ->top = np;
+    }
 
-   /* will pop the top value off of the stack */
-   StackData pop(Stack stack) {
-	  StackData temp = malloc(sizeof (StackData));
-	  if (isEmpty(stack)) { // if stack is empty.
-		 temp->error = true; // as this is the error case
-		 temp->i = stack->top->data->i;
-		 return temp;
-	  } else { // if no error is found
-		 NodePtr nodeTemp = stack ->top; // create temp of the head node
-		 temp->i = stack->top->data->i; // set the data to be returned
-		 temp->error = false; // set error code again to be sure.
-		 stack->top = stack->top->next; // move stack to the next value down
-		 free(nodeTemp); // free the previous head node
-		 return temp;
-	  }
+    /* will pop the top value off of the stack */
+    StackData pop(Stack stack) {
+        StackData temp = malloc(sizeof (StackData));
+        NodePtr nodeTemp = (NodePtr) malloc(sizeof (Node));
+        if (isEmpty(stack)) { // if stack is empty.
+            temp->error = true; // as this is the error case
+            temp->i = 0;
+            return temp;
+        } else { // if no error is found
+            nodeTemp = stack ->top; // create temp of the head node
+            temp->i = stack->top->data->i; // set the data to be returned
+            temp->error = false; // set error code again to be sure.
+            stack->top = stack->top->next; // move stack to the next value down
+            free(nodeTemp); // free the previous head node
+            return temp;
+        }
 
-   }
+    }
 
-   StackData peek(Stack stack) {
-	  return stack->top->data;
-   }
+    /* Returns the value current at the head of the stack without removing it */
+    StackData peek(Stack stack) {
+        return stack->top->data;
+    }
 
-   /* clear entire stack */
-   void empty(Stack stack) {
-	  stack->top = NULL;
-   }
+    /* clear entire stack */
+    void empty(Stack stack) {
+        stack->top = NULL;
+    }
 
-   /* function will return true if there are no values in stack */
-   bool isEmpty(Stack stack) {
-	  if (stack ->top == NULL) {
-		 return true;
-	  } else {
-		 return false;
-	  }
-   }
+    /* function will return true if there are no values in stack */
+    bool isEmpty(Stack stack) {
+        if (stack ->top == NULL) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-   void display(Stack stack) {
-	  /* Nothing here for now */
-   }
+    void display(Stack stack) {
+        /* Nothing here for now */
+    }
 
 
 
--- stacks/stackTest.c
+++ stacks/stackTest.c
@@ -12,9 +12,9 @@
 // This is interchangeable depending on what datatype is needed.
 
 typedef struct {
-   int i;
-   bool error; // instead of having a rouge value use error codes
-} Data,*StackData;
+    int i;
+    bool error; // instead of having a rouge value use error codes
+} Data, *StackData;
 
 #include "stackL.h"
 
@@ -22,51 +22,71 @@ typedef struct {
 
 int main(int argc, char** argv) {
 
-
-   // test out shitty stack.h functions here
-
-
-   // input from a file
-   FILE *ptr_file;
-   char buffer[MAXFILESIZE];
+    // input from a file
+    FILE *ptr_file;
+    char buffer[MAXFILESIZE];
 
 
-   ptr_file = fopen("expressions.txt", "r");
-   if (!ptr_file) { // if error is encountered or file does not exist
-	  return 1;
-   } else { // if no error is encountered
-	  while (fgets(buffer, MAXFILESIZE, ptr_file) != NULL) { // get the entire file and put it into an array.
-		 printf("%s", buffer); // print out entire file which is stored in buffer
-	  }
-   }
+    ptr_file = fopen("expressions.txt", "r");
+    if (!ptr_file) { // if error is encountered or file does not exist
+        return 1;
+    } else { // if no error is encountered
+        while (fgets(buffer, MAXFILESIZE, ptr_file) != NULL) { // get the entire file and put it into an array.
+            printf("%s", buffer); // print out entire file which is stored in buffer
+        }
+    }
 
-   // tokenize the input of the file into lines
-   // push all matching symbols to the stack
-   // pop all matching symbols from the stack 
-   // print what remains at the end of the line
-   // empty stack
-   // repeat for all lines in file.
-   
-   StackData someData = malloc(sizeof (StackData)); // must allocate memory space first
-   someData->i = 5;
-   someData->error = false;
-
-   Stack stack = initStack(); // initalize stack
-   empty(stack);
-   push(stack, someData);
-   someData->i = 3;
-   push(stack, someData);
-   someData = pop(stack);
-
-   printf("popped value = %d\n", someData->i);
-   someData = pop(stack);
-   printf("popped value again = %d", someData->i);
+    // tokenize the input of the file into lines
+    // push all matching symbols to the stack
+    // pop all matching symbols from the stack 
+    // print what remains at the end of the line
+    // empty stack
+    // repeat for all lines in file.
 
 
+    StackData temp = malloc(sizeof (StackData));
+    temp->error = false;
+
+    Stack stack = initStack(); // initalize stack
+    empty(stack); // ensure its empty
+
+
+    int x = 0;
+    for (x = 0; x < 10; x++) {
+        StackData temp = malloc(sizeof (StackData)); // you need to allocate the memory for each new value
+        temp ->i = x; // set values.
+        temp ->error = false;
+        push(stack, temp);
+    }
+
+
+    temp = pop(stack);
+    if (temp -> error) {
+        printf("error");
+    } else {
+        printf("popped value = %d\n", temp->i);
+    }
+
+    temp = pop(stack);
+    if (temp -> error) {
+        printf("error");
+    } else {
+        printf("popped value = %d\n", temp->i);
+    }
+
+    temp = pop(stack);
+    if (temp -> error) {
+        printf("error");
+    } else {
+        printf("popped value = %d\n", temp->i);
+    }
+
+    StackData temp2 = peek(stack);
+    printf("peeked value = %d\n", temp2->i);
 
 
 
-   fclose(ptr_file);
-   return (EXIT_SUCCESS);
+    fclose(ptr_file);
+    return (EXIT_SUCCESS);
 }
 
